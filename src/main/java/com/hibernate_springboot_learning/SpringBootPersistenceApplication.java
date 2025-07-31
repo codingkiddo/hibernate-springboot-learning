ece40e4adef0903472792863092b6324a94cd32a
Vinod Kumar <vinodkumar@192.168.1.3>
2025-07-31 20:19:52 +0530

    Initial Commit

---
+++ src/main/java/com/hibernate_springboot_learning/SpringBootPersistenceApplication.java
@@ -0,0 +1,11 @@
+package com.hibernate_springboot_learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPersistenceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPersistenceApplication.class, args);
+	}
+}
